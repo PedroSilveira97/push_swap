cb49101f8c3eb37250acff3d3e49f1daffa1e650
Pedro Silveira <pedrotsilveira@gmail.com>
2025-09-24 23:14:01 +0100

    push_swap

--- Sources/radix.c
+++ Sources/radix.c
@@ -6,69 +6,11 @@
 /*   By: ptavares <ptavares@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 15:45:45 by ptavares          #+#    #+#             */
-/*   Updated: 2025/09/24 22:56:15 by ptavares         ###   ########.fr       */
+/*   Updated: 2025/09/24 23:13:18 by ptavares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
-
-static int	highest_set_bit_index(int n)
-{
-	int	bit;
-
-	bit = 0;
-	while ((n >> bit) != 0)
-		bit++;
-	if (bit == 0)
-		return (0);
-	return (bit -1);
-}
-
-static int	max_rank(t_node	*head)
-{
-	int	max;
-
-	if (!head)
-		return (0);
-	max = head->index;
-	head = head->next;
-	while (head)
-	{
-		if (head->index > max)
-			max = head->index;
-		head = head->next;
-	}
-	return (max);
-}
-
-void	binary_radix(t_node **stack_a, t_node **stack_b)
-{
-	int	max;
-	int	maxbit;
-	int	bit;
-	int	size;
-	int	i;
-
-	max = max_rank(*stack_a);
-	maxbit = highest_set_bit_index(max);
-	size = stack_size(*stack_a);
-	bit = 0;
-	while (bit <= maxbit)
-	{
-		i = 0;
-		while (i < size)
-		{
-			if (((*stack_a)->index >> bit & 1) == 0)
-				pb(stack_a, stack_b);
-			else
-				ra(stack_a);
-			i++;
-		}
-		while (*stack_b)
-			pa(stack_a, stack_b);
-		bit++;
-	}
-}
 
 int	pos_of_max(t_node *stack)
 {
@@ -113,4 +55,41 @@ void	push_back_desc(t_node **stack_a, t_node **stack_b)
 		}
 		pa(stack_a, stack_b);
 	}
+}
+
+void	chunk_sort(t_node **stack_a, t_node **stack_b, int chunk)
+{
+	int	pushed;
+
+	pushed = 0;
+	while (*stack_a)
+	{
+		if ((*stack_a)->index <= pushed)
+		{
+			pb(stack_a, stack_b);
+			rb(stack_b);
+			pushed++;
+		}
+		else if ((*stack_a)->index <= pushed + chunk)
+		{
+			pb(stack_a, stack_b);
+			pushed++;
+		}
+		else
+			ra(stack_a);
+	}
+	push_back_desc(stack_a, stack_b);
+}
+
+void	radix_sort(t_node **stack_a, t_node **stack_b)
+{
+	int	size;
+
+	size = stack_size(*stack_a);
+	if (size <= 5)
+		return ;
+	if (size <= 100)
+		chunk_sort(stack_a, stack_b, 15);
+	else
+		chunk_sort(stack_a, stack_b, 30);
 }
